d6276a5454ba78aa9f38247fc2df9b7e4a09a948
ramkrsn <guungramaa@gmail.com>
2024-04-18 12:18:15 +0700

    edit comman csss

--- resources/views/Melihat_Jadwal_Dokter.blade.php
+++ resources/views/Melihat_Jadwal_Dokter.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jadwal Dokter Fikri Medika</title>
-    <link rel="stylesheet" href="css/JadwalDokter.css">
+    <link rel="stylesheet" href="resources/css/JadwalDokter.css">
 </head>
 <body>
     <div class="container">
