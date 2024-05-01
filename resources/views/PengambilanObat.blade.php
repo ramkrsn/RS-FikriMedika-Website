a8f65183dc2b840bdb7a09c9373f6ceabf9c7b65
IDewaAgungRama <idewaagungrama@student.telkomuniversity.ac.id>
2024-05-01 15:57:25 +0700

    no message

---
+++ resources/views/PengambilanObat.blade.php
@@ -0,0 +1,72 @@
+<div style="width: 100%; height: 100%; position: relative; background: #FAF8ED">
+    <div style="width: 390px; height: 54px; padding-top: 14.50px; padding-bottom: 8.50px; padding-left: 120px; padding-right: 120px; left: 0px; top: 806px; position: absolute; background: rgba(248, 248, 248, 0.82); box-shadow: 0px -0.30000001192092896px 0px #AEAEB4; backdrop-filter: blur(59.80px); flex-direction: column; justify-content: space-between; align-items: center; display: inline-flex">
+        <div style="flex-direction: column; justify-content: flex-start; align-items: center; gap: 14px; display: flex">
+            <div style="height: 11px; justify-content: center; align-items: center; gap: 5px; display: inline-flex">
+                <div style="width: 5.87px; height: 8.57px; background: rgba(0, 0, 0, 0.50)"></div>
+                <div style="text-align: center; color: rgba(0, 0, 0, 0.80); font-size: 12px; font-family: Inter; font-weight: 400; line-height: 12px; word-wrap: break-word">https://www.rsfikrimedika.com/</div>
+            </div>
+            <div style="width: 135px; height: 5px; background: black; border-radius: 100px"></div>
+        </div>
+    </div>
+    <div style="width: 360px; height: 100px; left: 15px; top: 281px; position: absolute">
+        <div style="width: 360px; height: 100px; left: 0px; top: 0px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 10px"></div>
+        <div style="left: 113px; top: 56px; position: absolute; color: black; font-size: 8px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Paracetamol 600mg, Dexamethasone 0,5mg</div>
+        <div style="left: 113px; top: 32px; position: absolute; color: black; font-size: 14px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Resep 001</div>
+    </div>
+    <div style="width: 360px; height: 100px; left: 15px; top: 281px; position: absolute">
+        <div style="width: 360px; height: 100px; left: 0px; top: 0px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 10px"></div>
+        <div style="left: 113px; top: 56px; position: absolute; color: black; font-size: 8px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Paracetamol 600mg, Dexamethasone 0,5mg</div>
+        <div style="left: 113px; top: 32px; position: absolute; color: black; font-size: 14px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Resep 001</div>
+        <img style="width: 100px; height: 100px; left: 0px; top: 0px; position: absolute; border-radius: 10px" src="https://via.placeholder.com/100x100" />
+    </div>
+    <div style="width: 360px; height: 100px; left: 15px; top: 391px; position: absolute">
+        <div style="width: 360px; height: 100px; left: 0px; top: 0px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 10px"></div>
+        <div style="left: 113px; top: 56px; position: absolute; color: black; font-size: 8px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Paracetamol 600mg, Dexamethasone 0,5mg</div>
+        <div style="left: 113px; top: 32px; position: absolute; color: black; font-size: 14px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Resep 002</div>
+        <img style="width: 100px; height: 100px; left: 0px; top: 0px; position: absolute; border-radius: 10px" src="https://via.placeholder.com/100x100" />
+    </div>
+    <div style="width: 360px; height: 100px; left: 15px; top: 501px; position: absolute">
+        <div style="width: 360px; height: 100px; left: 0px; top: 0px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 10px"></div>
+        <div style="left: 113px; top: 56px; position: absolute; color: black; font-size: 8px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Paracetamol 600mg, Dexamethasone 0,5mg</div>
+        <div style="left: 113px; top: 32px; position: absolute; color: black; font-size: 14px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Resep 002</div>
+        <img style="width: 100px; height: 100px; left: 0px; top: 0px; position: absolute; border-radius: 10px" src="https://via.placeholder.com/100x100" />
+    </div>
+    <div style="width: 360px; height: 100px; left: 15px; top: 611px; position: absolute">
+        <div style="width: 360px; height: 100px; left: 0px; top: 0px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 10px"></div>
+        <div style="left: 113px; top: 56px; position: absolute; color: black; font-size: 8px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Paracetamol 600mg, Dexamethasone 0,5mg</div>
+        <div style="left: 113px; top: 32px; position: absolute; color: black; font-size: 14px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Resep 002</div>
+        <img style="width: 100px; height: 100px; left: 0px; top: 0px; position: absolute; border-radius: 10px" src="https://via.placeholder.com/100x100" />
+    </div>
+    <div style="width: 390px; height: 143px; left: 0px; top: 0px; position: absolute">
+        <div style="width: 390px; height: 143px; left: 0px; top: 0px; position: absolute; background: white"></div>
+        <img style="width: 199px; height: 44px; left: 27px; top: 75px; position: absolute" src="https://via.placeholder.com/199x44" />
+        <div style="width: 25px; height: 19.44px; left: 329px; top: 87px; position: absolute">
+            <div style="width: 25px; height: 0px; left: 0px; top: 0px; position: absolute; border: 3.47px #21BF73 solid"></div>
+            <div style="width: 25px; height: 0px; left: 0px; top: 9.72px; position: absolute; border: 3.47px #21BF73 solid"></div>
+            <div style="width: 25px; height: 0px; left: 0px; top: 19.44px; position: absolute; border: 3.47px #21BF73 solid"></div>
+        </div>
+    </div>
+    <div style="width: 390px; height: 55px; padding-top: 25px; padding-bottom: 15px; padding-left: 34px; padding-right: 14px; left: 0px; top: 0px; position: absolute; justify-content: space-between; align-items: flex-end; display: inline-flex">
+        <div style="width: 26.62px; height: 10.36px; background: black"></div>
+        <div style="width: 66.60px; justify-content: center; align-items: center; gap: 5px; display: flex">
+            <img style="width: 17px; height: 10.67px" src="https://via.placeholder.com/17x11" />
+            <img style="width: 15.27px; height: 10.97px" src="https://via.placeholder.com/15x11" />
+            <div style="width: 24.33px; height: 11.33px; position: relative">
+                <img style="width: 24.33px; height: 11.33px; left: 0px; top: 0px; position: absolute; opacity: 0.40" src="https://via.placeholder.com/24x11" />
+                <div style="width: 18px; height: 7.33px; left: 2px; top: 2px; position: absolute; background: black; border-radius: 1.33px"></div>
+            </div>
+        </div>
+    </div>
+    <div style="width: 390px; height: 59px; left: 0px; top: 143px; position: absolute; background: #F1F864"></div>
+    <div style="left: 98px; top: 158px; position: absolute; text-align: center; color: #21BF73; font-size: 20px; font-family: Poppins; font-weight: 700; line-height: 30px; word-wrap: break-word">Pengambilan Obat</div>
+    <div style="width: 358px; height: 0px; left: 16px; top: 252px; position: absolute; border: 0.50px black solid"></div>
+    <div style="width: 107px; height: 30px; left: 21px; top: 212px; position: absolute">
+        <div style="width: 107px; height: 30px; left: 0px; top: 0px; position: absolute; background: #21BF73; border-radius: 10px"></div>
+        <div style="left: 33px; top: 4px; position: absolute; text-align: center; color: white; font-size: 14px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Resep</div>
+    </div>
+    <div style="width: 107px; height: 30px; left: 135px; top: 212px; position: absolute">
+        <div style="width: 107px; height: 30px; left: 0px; top: 0px; position: absolute; background: white; border-radius: 10px"></div>
+        <div style="left: 29px; top: 4px; position: absolute; text-align: center; color: #21BF73; font-size: 14px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Antrian</div>
+    </div>
+    <div style="width: 358px; height: 0px; left: 16px; top: 252px; position: absolute; border: 0.50px black solid"></div>
+</div>
