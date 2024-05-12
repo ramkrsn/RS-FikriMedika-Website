892c2148bf3356f1699119c7ef411a9e33a4ad65
drmdvld <razanilzan@gmail.com>
2024-05-12 14:35:17 +0700

    update profil rumah sakit (sprint 1), antrian (sprint 2)

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProfilRumahSakitController;
-
-use App\Http\Middleware\AuthMiddleware;
-use App\Http\Middleware\GuestMiddleware;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-Route::get('/', [ProfilRumahSakitController::class, 'index'])->name('ProfilRumahSakit');
+use App\Http\Controllers\AntrianController;
 
 Route::get('/', function () {
-    return view('ProfilRumahSakit');
+    return view('profil');
 });
+
+Route::get('/queue', [AntrianController::class, "index"])->name('queue.index');
+Route::get('/queue/{id}', [AntrianController::class, "show"])->name('queue.show');
