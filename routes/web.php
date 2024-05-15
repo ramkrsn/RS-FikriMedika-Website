7991179d7cd0250d4890812f7b882a7eac163c72
IDewaAgungRama <idewaagungrama@student.telkomuniversity.ac.id>
2024-05-15 20:37:10 +0700

    upload

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\AntrianController;
 
 Route::get('/', function () {
-    return view('informasiobat');
+    return view('welcome');
 });
-Route::get('/artikel', function () {
-    return view('artikel');
-})->name('artikel');
 
-Route::get('/artikel', 'ArtikelController@index')->name('artikel');
-
-Route::get('/artikel', 'App\Http\Controllers\ArtikelController@index')->name('artikel');
-
-Route::get('/', function() {
-    return view('Melihat_Jadwal_Dokter');
-});
+Route::get('/queue', [AntrianController::class, "index"])->name('queue.index');
+Route::get('/queue/{id}', [AntrianController::class, "show"])->name('queue.show');
